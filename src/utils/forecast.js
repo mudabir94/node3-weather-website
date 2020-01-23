66fdfdfd8321d70d51c72ac8a4e54a9d10bf75e3
mudabir94 <mudabirahmad1994@gmail.com>
2020-01-23 08:49:43 +0500

    Add High and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request= require("request")
+
 const forecast = (lat,long,callback) => {
     const url ='https://api.darksky.net/forecast/0d423de6243fb0e9a9d2b09f2c262324/'+lat+','+long+ '?units=si'
     request({url,json:true},(error,{body}) => {
@@ -8,8 +9,7 @@ const forecast = (lat,long,callback) => {
             callback("Unable to find location. Try another search.",undefined)
         }
         else {
-
-            callback(undefined,body.daily.data[0].summary +"it is currently "+body.currently.apparentTemperature+" degrees out. There is a " +body.currently.precipProbability+ " of rain.")
+            callback(undefined,body.daily.data[0].summary +"it is currently "+body.currently.apparentTemperature+" degrees out. The high today is "+body.daily.data[0].temperatureHigh +". The Low Today is "+body.daily.data[0].temperatureLow+". There is a " +body.currently.precipProbability+ " of rain. ")
         }
     })
 }
